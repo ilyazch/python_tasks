dddc664357aa088eb1332464e8a4f0a54f360897
ilyazch <ilyaaylitms@gmail.com>
2020-07-29 02:53:05 +0300

    init test

---
+++ task_3_1.py
@@ -0,0 +1,5 @@
+a = int(input("введи число:"))
+if a % 1000 == 0:
+    print('millennium')
+else:
+    print('не делится')
---
+++ task_3_2.py
@@ -0,0 +1,7 @@
+a=int(input('Кол-во людей:'))
+if a > 50:
+    print('ресторан')
+elif 50>a>20:
+    print('кафе')
+else:
+    print('дом')
